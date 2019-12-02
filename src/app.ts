2a2f20471164fa68cae4b771edb08719d9794a02
Steven Wang <steven.guan.wang@gmail.com>
2019-12-02 20:08:46 +1000

    demo code

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { InversifyExpressServer } from 'inversify-express-utils';
 import { Container } from 'inversify';
 
 import './api-modules/employees/loader';
+import createMiddleware from 'swagger-express-middleware';
 import { registerDomainServices, registerResourceAccessServices } from './api/modularity';
 import { registerErrorHandlers } from './api/error-handler';
 
@@ -15,6 +16,21 @@ export const buildServer = (container: Container) => {
   server.setConfig((app) => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+
+    console.log('start register swagger middleware')
+    createMiddleware('swagger.yaml', app, function (err, middleware) {
+      // refer page https://www.npmjs.com/package/swagger-express-middleware#installation-and-use
+      app.use(
+        middleware.metadata(),
+        middleware.CORS(),
+        middleware.files(),
+        middleware.parseRequest(),
+        middleware.validateRequest(),
+        middleware.mock()
+      );
+      console.log('end register swagger middleware')
+    });
+    console.log('proceed to constroller registration')
   });
 
   server.setErrorConfig(registerErrorHandlers);
